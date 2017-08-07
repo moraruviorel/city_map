6139b460f72b3a823e9c3b672e001e5e6f342386
Moraru Viorel <VMoraru@ASP>
2017-08-08 00:32:41 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCYCUupSZTCKbF3FXPc6T9zOBhyid9GU5c'
+    })
+  ],
+  providers: [],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {}
